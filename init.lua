d3a2f4f6d7ddb73988934ea9fdc6dbdd43b21f66
Tim7900 <100736284+Tim79000@users.noreply.github.com>
2023-06-10 11:33:44 -0800

    Add stripped logs and bark
    Current code is a bit hacky and I'm going to improve it later, but it works as intended.

---
+++ init.lua
@@ -0,0 +1,9 @@
+local include, nodecore
+    = include, nodecore
+
+local modname = minetest.get_current_modname()
+
+
+
+include("stripped_log")
+-- include("plank") -- Has to be enabled using a settings type.
---
+++ stripped_log.lua
@@ -0,0 +1,71 @@
+local include, nodecore
+    = include, nodecore
+
+local modname = minetest.get_current_modname()
+
+
+minetest.register_node(modname .. ":stripped_log", {
+		description = "Stripped Log",
+		tiles = {
+			modname .. "_stripped_tree_top.png",
+			modname .. "_stripped_tree_top.png",
+			modname .. "_stripped_tree_side.png"
+		},
+		groups = {
+			choppy = 2,
+			flammable = 8,
+			fire_fuel = 6,
+			log = 1,
+		},
+		sounds = nodecore.sounds("nc_tree_woody"),
+		paramtype2 = "facedir",
+		on_place = minetest.rotate_node
+	})
+
+minetest.register_craftitem(modname..":bark", {
+	description = "Bark",
+	inventory_image = "nc_tree_tree_side.png^[mask:" .. modname .. "_bark.png",
+	wield_image = "nc_tree_tree_side.png^[mask:" .. modname .. "_bark.png",
+	sounds = nodecore.sounds("nc_tree_woody"),
+	groups = {
+		choppy = 1,
+		bark = 1
+		}
+})
+
+nodecore.register_craft({
+		label = "debark log",
+		action = "pummel",
+		toolgroups = {choppy = 2},
+--                normal {y = normaly},
+		nodes = {
+			{match = "nc_tree:log", replace = modname .. ":stripped_log"}
+		},
+		items = {
+			{name = modname .. ":bark", count = 4, scatter = 5}
+		},
+	})
+
+-- Overrides the vanilla recipe to fix a bug.
+
+local function splitrecipe(choppy, normaly)
+	nodecore.register_craft({
+			label = "split tree to planks",
+			action = "pummel",
+			toolgroups = {choppy = choppy},
+			normal = {y = normaly},
+			check = function(_, data)
+				local y = nodecore.facedirs[data.node.param2].t.y
+				return y == 1 or y == -1
+			end,
+			indexkeys = {"group:log"},
+			nodes = {
+				{match = {groups = {log = true}}, replace = "air"}
+			},
+			items = {
+				{name = "nc_woodwork:plank", count = 4, scatter = 5}
+			}
+		})
+end
+splitrecipe(1, 1)
+splitrecipe(4, -1)
